4bd41fe1843659a6fa43fba810b67b75adc0d129
Wiktor <147401344+ASzazX@users.noreply.github.com>
2024-03-05 11:33:02 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
-if [ -f "$file" ]; then
+#!/bin/bash
+
+ # Find all C files recursively in subdirectories
+ find . -name '*.c' | while read file; do
+     if [ -f "$file" ]; then
          filename=$(basename "$file" .c)
          output_path=$(dirname "$file")
          clang -o "${output_path}/${filename}" "$file" -lm
@@ -6,3 +10,8 @@ if [ -f "$file" ]; then
          if [ $? -eq 0 ]; then
              echo "Compilation successful for $file"
          else
+             echo "Error compiling $file"
+             exit 1 # Exit the pipeline with a non-zero status code
+         fi
+     fi
+ done
